bc0b188faa49fa968a0f25488e182f7c3fac310b
Thomas Nipen <tnipen@gmail.com>
2018-05-02 13:54:28 +0200

    Allow location metadata file as location input in download

--- met2verif/download.py
+++ met2verif/download.py
@@ -17,8 +17,9 @@ import met2verif.version
 
 def add_subparser(parser):
    subparser = parser.add_parser('download', help='Downloads KDVH data')
-   subparser.add_argument('-l', type=str, help='Location ids. Either a comma-separated list of ids (e.g. 18700,50540), a filename with list of stations (new-line, comma-, and/or space-separated), or unspecified (all stations downloaded).', dest="locations")
+   subparser.add_argument('-l', type=str, help='Location ids. Either a comma-separated list of ids (e.g. 18700,50540), a filename with list of stations (new-line, comma-, and/or space-separated), a locations metadata file, or unspecified (all stations downloaded).', dest="locations")
    subparser.add_argument('-o', metavar="FILE", help='Output file', dest="filename", required=True)
+   subparser.add_argument('-i', type=met2verif.util.parse_numbers, help='Comma-separated list of hours of the day to download. If unspecified, download all hours.', dest="hours")
    subparser.add_argument('-sd', type=str, help='Start date (yyyymmdd)', dest="sd", required=True)
    subparser.add_argument('-ed', type=str, help='End date (yyyymmdd)', dest="ed", required=True)
    subparser.add_argument('-v', type=str, help='List of variables (e.g. TA,FF)', dest="variables", required=True)
@@ -37,13 +38,19 @@ def run(parser):
    url = "%s&fd=%s&td=%s" % (baseurl, start_date, end_date)
    for variable in args.variables.split(','):
       url += "&p=%s" % variable
+   if args.hours is not None:
+      for hour in args.hours:
+         url += "%h=%d" % hour
    if args.locations is not None:
       if os.path.exists(args.locations):
          # Read from file
-         file = open(args.locations, 'r')
-         ids = list()
-         for line in file:
-            ids += [int(word) for word in re.split(',| ', line)]
+         try:
+            ids = met2verif.locinput.get(args.locations).read().keys()
+         except Exception as e:
+            file = open(args.locations, 'r')
+            ids = list()
+            for line in file:
+               ids += [int(word) for word in re.split(',| ', line)]
       else:
          ids = [int(id) for id in args.locations.split(',')]
 
