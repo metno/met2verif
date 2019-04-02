bc27995f3e9a5588bdaf30197ad176f0e3bc9ec0
Thomas Nipen <tnipen@gmail.com>
2019-04-02 07:57:21 +0000

    Allow verif files as location input

--- met2verif/locinput.py
+++ met2verif/locinput.py
@@ -1,20 +1,41 @@
-import met2verif.util
+import sys
+import os
 import numpy as np
+import netCDF4
+
+
+import met2verif.util
+import verif.input
 
 
 def get(filename):
-    file = open(filename, 'r')
-    for i in range(5):
-        header = file.readline()
-        if len(header) < 5:
-            continue
-        else:
-            if header[0:5] == "DEPAR":
-                file.close()
-                return Kdvh(filename)
+    _stderr = sys.stderr
+    _stdout = sys.stdout
+    try:
+        # Avoid showing error message
+        null = open(os.devnull,'wb')
+        sys.stdout = sys.stderr = null
+
+        file = verif.input.get_input(filename)
+        f = Verif(filename)
+        sys.stderr = _stderr
+        sys.stdout = _stdout
+        return f
+    except:
+        sys.stderr = _stderr
+        sys.stdout = _stdout
+        file = open(filename, 'r')
+        for i in range(5):
+            header = file.readline()
+            if len(header) < 5:
+                continue
             else:
-                file.close()
-                return Comps(filename)
+                if header[0:5] == "DEPAR":
+                    file.close()
+                    return Kdvh(filename)
+                else:
+                    file.close()
+                    return Comps(filename)
     raise NotImplementedError
 
 
@@ -22,8 +43,20 @@ class LocInput(object):
     def __init__(self, filename):
         self.filename = filename
 
-    def read(self, filename):
+    def read(self):
         raise NotImplementedError
+
+
+class Verif(LocInput):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read(self):
+        file = verif.input.get_input(self.filename)
+        locations = dict()
+        for location in file.locations:
+            locations[location.id] = {"lat": location.lat, "lon": location.lon, "elev": location.elev}
+        return locations
 
 
 class Kdvh(LocInput):
